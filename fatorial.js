97e95007a03ca3301a4baeac33f4902834a51d15
GabiGow011 <143434640+GabiGow011@users.noreply.github.com>
2023-09-21 16:41:14 -0300

    Add files via upload

---
+++ fatorial.js
@@ -0,0 +1,19 @@
+const fatorialUm = Number(5 * 5)
+
+console.log(fatorialUm)
+
+const fatorialDois = Number(5 * 4)
+
+console.log(fatorialDois)
+
+const fatorialTres = Number(5 * 3)
+
+console.log(fatorialTres)
+
+const fatorialQuatro = Number(5 * 2)
+
+console.log(fatorialQuatro)
+
+const fatorialCinco = Number(5 * 1)
+
+console.log(fatorialCinco)
